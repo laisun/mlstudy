ed149a5e967e47c426b3896eb3c1280ef39bf968
alonsoli <alonsoli@tencent.com>
2016-11-04 05:29:49 -0700

    add

--- sklearn/sk_tree_lr.py
+++ sklearn/sk_tree_lr.py
@@ -23,7 +23,7 @@ class RF_LR(object):
 
   """
 
-  def __init__(self,tree='rf',n_estimator=100,max_depth=3):
+  def __init__(self,n_estimator=100,max_depth=3):
     """ make a initiation of the models.
 
     Parameters
@@ -33,10 +33,7 @@ class RF_LR(object):
 
     """
 
-    if tree == 'rf':
-      self.trees = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimator)
-    else:
-      self.trees = GradientBoostingClassifier(n_estimators=n_estimator)	
+    self.trees = RandomForestClassifier(n_estimators=n_estimator,max_depth=max_depth)
     self.encoder = preprocessing.OneHotEncoder()
     self.lr = linear_model.LogisticRegression()
 
@@ -55,8 +52,9 @@ class RF_LR(object):
     self.encoder.fit(self.trees.apply(X_train))
 
     self.lr.fit(
-            self.encoder.transform(self.trees.apply(X_train)), 
-            y_train)
+             self.encoder.transform(self.trees.apply(X_train)), 
+             y_train
+	    )
   
   def predict(self,X_test):
     """Predict class for X.
@@ -74,6 +72,104 @@ class RF_LR(object):
 
     y_pred = self.lr.predict(
                  self.encoder.transform(self.trees.apply(X_test))
+	       )
+
+    return y_pred
+
+  def scores(self,X_test,y_test):
+    """make a evaluation of the model.
+
+    Parameters
+    -----------
+    X_test : array with shape = [n_samples, n_features],
+              The input samples. Internally, its dtype will 
+	      be converted to ``dtype=np.float32``.
+    y_test : array with shape = [n_samples, 1] ,its dtype is int.
+   
+
+    Returns
+    -------
+    
+    average_precision_score : float
+       The the area under the precision-recall curve.
+    
+    roc_auc_score : float
+        Area Under the Curve (AUC).
+    
+    precision_score: float
+        The precision, TP/(TP+FP)
+
+    """
+
+    y_pred = self.predict(X_test)
+    average_precision_score = metrics.average_precision_score(y_test, y_pred)
+    roc_auc_score = metrics.roc_auc_score(y_test, y_pred)
+    precision_score = metrics.precision_score(y_test, y_pred)
+
+    return average_precision_score,roc_auc_score,precision_score
+
+
+class GBDT_LR(object):
+  """Tree model plus Logistic Regression.
+
+  Parameters
+  ----------
+  trees : the tree model to make feature transformation.
+          
+  encoder : the feature encoder to encode the result of trees.
+
+  lr : the classification model,using the result of encoder.
+
+  """
+
+  def __init__(self,n_estimator=100,max_depth=3):
+    """ make a initiation of the models.
+
+    Parameters
+    -----------
+    n_estimator : the number of sub-trees.
+    max_depth : the max depth of each sub-tree.
+
+    """
+
+    self.trees = GradientBoostingClassifier(n_estimators=n_estimator)
+    self.encoder = preprocessing.OneHotEncoder()
+    self.lr = linear_model.LogisticRegression()
+
+  def fit(self,X_train,y_train):
+    """train the models.
+
+    Parameters
+    -----------
+    X_train : array with shape = [n_samples, n_features],
+              The input samples. Internally, its dtype will 
+	      be converted to ``dtype=np.float32``.
+    y_train : array with shape = [n_samples, 1] ,its dtype is int.
+
+    """
+    self.trees.fit(X_train, y_train)
+    self.encoder.fit(self.trees.apply(X_train)[:, :, 0])
+
+    self.lr.fit(
+            self.encoder.transform(self.trees.apply(X_train)[:, :, 0]), 
+            y_train)
+  
+  def predict(self,X_test):
+    """Predict class for X.
+      the predicted class is the one with highest probability.
+
+    Parameters
+    ----------
+    X_test : array with shape = [n_samples, n_features],its dtype is float32.
+
+    Returns
+    -------
+    y : array of shape = [n_samples,1]
+
+    """
+
+    y_pred = self.lr.predict(
+                 self.encoder.transform(self.trees.apply(X_test)[:, :, 0])
 	       )
 
     return y_pred
